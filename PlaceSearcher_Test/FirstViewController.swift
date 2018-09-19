d44b0a5d962248d132f99544f4269189d1326d63
Alexandr Kurdyukov <psychobumblebee@gmail.com>
2018-09-19 10:55:14 +0300

    Initial Commit

---
+++ PlaceSearcher_Test/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PlaceSearcher_Test
+//
+//  Created by Alexandr Kurdyukov on 19.09.2018.
+//  Copyright © 2018 Alexandr Kurdyukov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
