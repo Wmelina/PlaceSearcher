9bb7f2ccea80c3ea47ecb4f9226162e6fbfa77f6
Alexandr Kurdyukov <psychobumblebee@gmail.com>
2018-09-24 23:02:47 +0300

    start project

---
+++ PlaceSearcher_Test/Controllers/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  PlaceSearcher_Test
+//
+//  Created by Alexandr Kurdyukov on 24.09.2018.
+//  Copyright © 2018 Alexandr Kurdyukov. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+}
---
+++ PlaceSearcher_Test/Controllers/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  PlaceSearcher_Test
+//
+//  Created by Alexandr Kurdyukov on 24.09.2018.
+//  Copyright © 2018 Alexandr Kurdyukov. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+}
